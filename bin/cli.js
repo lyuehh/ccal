d9a551e13bb1b0d7c20475c92c96ab5f869f1f68
lyuehh <lyuehh@gmail.com>
2013-12-21 19:05:42 +0800

    part1, month now work

---
+++ bin/cli.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+var _ = require('underscore');
+var run = require('../lib/ccal.js').run;
+var support = ['2013', '2014'];
+
+var arg = process.argv[2];
+
+if (arg && (arg.length === 4 || arg.length === 6)) {
+    if (_.include(support, arg.toString().substr(0, 4))) {
+        run(arg);
+    } else {
+        console.log('now only support 2013, 2014');
+    }
+} else {
+  console.log('');
+  console.log('Usage: ccal [year | year_and_month]');
+  console.log('');
+  console.log('Example: ccal 2014, ccal 20140101 etc');
+  console.log('now only support 2013, 2014');
+  console.log('');
+}
