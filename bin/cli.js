cf3e07ac3fddad6c96144f816ceab238c544036b
lyuehh <lyuehh@gmail.com>
2013-12-22 13:00:06 +0800

    now basic work

--- bin/cli.js
+++ bin/cli.js
@@ -11,11 +11,17 @@ if (arg && (arg.length === 4 || arg.length === 6)) {
     } else {
         console.log('now only support 2013, 2014');
     }
-} else {
+} else if(arg === '-h'){
   console.log('');
   console.log('Usage: ccal [year | year_and_month]');
   console.log('');
   console.log('Example: ccal 2014, ccal 20140101 etc');
   console.log('now only support 2013, 2014');
   console.log('');
+} else {
+    var d = new Date();
+    var year = d.getFullYear();
+    var month = d.getMonth() + 1;
+    var arg = year + ('00'+month).substr(-2);
+    run(arg);
 }
